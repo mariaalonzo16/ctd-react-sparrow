afb25743d8d5b925cced41662365f1d0ee02398f
Maria Jose Alonzo Claudino <mjaclaudino16@gmail.com>
2022-04-02 19:39:40 -0400

    updated

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -2,13 +2,28 @@ import React from "react";
 
 function AddTodoForm() {
 
+    const handleAddTodo = (e) => {
+        e.preventDefault();
+
+        const todoTitle = e.target.value;
+
+        console.log(todoTitle); 
+      
+    }
+
+  
+    
     return(
-        <form>
-            <label htmlFor="todoTitle">Title</label>
-            <input id="todoTitle"></input>
+        <form onSubmit={handleAddTodo}>
+            <label htmlFor="todoTitle">Title: </label>
+            <input id="todoTitle" name="title" ></input>
             <button>Submit</button>
         </form>
     );
 }
 
 export default AddTodoForm
+
+
+
+
